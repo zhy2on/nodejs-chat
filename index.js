619b67092451a4799473372eafa19368f8f41b23
zhy2on <dhwlgus5940@gmail.com>
2022-08-22 01:42:25 +0900

    Feat: 서버 & 클라이언트가 연결된 모든 유저를 공유하도록 구현 완료

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const WebSocket = require("ws");
-const ws = new WebSocket.Server({ port:8008 });
+const ws = new WebSocket.Server({ port: 8008 });
 
-ws.on("connection", function connect(ws, req) { // 웹소켓에 특정 클라이언트가 연결되었을 때 실행
-	console.log("NEW USER CONNECT");
+let user_id = 0; //클라이언트에게 부여되는 고유한 식별값
+let ALL_WS = []; //전체 유저들을 통제할 수 있도록 각 유저에 대한 websocket, user_id 저장
+
+ws.on("connection", function connect(websocket, req) { //웹소켓에 특정 클라이언트가 연결되었을 때 실행
+	user_id++;
+	console.log("NEW USER CONNECT (" + user_id + ")");
+	ALL_WS.push({ "ws": websocket, "user_id": user_id, "user_name": "" });
+
+	sendUserId(user_id);
+	websocket.on("message", function incomming(message) {
+		message = JSON.parse(message);
+		console.log(message);
+		switch (message.code) {
+			case "connect_name": //사용자 추가
+				ALL_WS.forEach(function (element, index) {
+					if (element.user_id == message.user_id) {
+						element.user_name = message.user_name;
+					}
+				});
+				sendAllUsers();
+				break;
+		}
+	})
+
+	function sendAllUsers() { //전체 사용자 정보를 보냄
+		let data = { "code": "all_users", "msg": JSON.stringify(ALL_WS) };
+
+		ALL_WS.forEach(function (element, index) {
+			element.ws.send(JSON.stringify(data));
+		});
+	}
+
+	function sendUserId(user_id) {
+		let data = { "code": "my_user_id", "user_id": user_id };
+		websocket.send(JSON.stringify(data));
+	}
 });
